dc23c527a65f4f212d557a284ce3917d646c561f
Bhavya <bhavyamishrasps@gmail.com>
2021-12-14 16:27:31 +0530

    initial commit

---
+++ src/main/java/com/javainuse/springboothelloworld/SpringbootHelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.javainuse.springboothelloworld;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		
+			System.out.println( "Hello World!" );
+		}
+}
+
